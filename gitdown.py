bb94049d50a7654bcd0e1d6772e0f84d73811401
pedroelir <pedro_r_b777@hotmail.com>
2023-04-23 14:13:42 +0200

    first Script

---
+++ gitdown.py
@@ -0,0 +1,28 @@
+import os
+from github import Github
+
+# replace with your GitHub access token
+ACCESS_TOKEN = os.environ.get("GH_TOKEN")
+
+# specify the repository and the directory to download
+REPO_NAME = "pedroelir/demos"
+DIRECTORY_PATH = "python_demo/Comm/utils/Server"
+
+# create a PyGithub instance with the access token
+g = Github(ACCESS_TOKEN)
+
+# get the repository object
+repo = g.get_repo(REPO_NAME)
+branch = "Python_Rpi"
+
+# get the GitTree object for the specified directory
+tree = repo.get_git_tree(sha=branch, recursive=True).tree
+for item in tree:
+    if item.path.startswith(DIRECTORY_PATH):
+        # download the file or directory
+        if item.type == "blob":
+            content = repo.get_contents(item.path, ref=branch).decoded_content
+            with open(item.path, "wb") as f:
+                f.write(content)
+        elif item.type == "tree":
+            os.makedirs(item.path, exist_ok=True)
