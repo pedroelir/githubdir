a5d2fdbf2101c083aafdc8522fabca3f3f22cacc
pedroelir <pedro_r_b777@hotmail.com>
2023-07-08 11:45:53 +0200

    update repo and path

--- gitdown.py
+++ gitdown.py
@@ -5,15 +5,15 @@ from github import Github
 ACCESS_TOKEN = os.environ.get("GH_TOKEN")
 
 # specify the repository and the directory to download
-REPO_NAME = "pedroelir/demos"
-DIRECTORY_PATH = "python_demo/Comm/utils/"
+REPO_NAME = "pedroelir/dir"
+DIRECTORY_PATH = "system/"
 
 # create a PyGithub instance with the access token
 g = Github(ACCESS_TOKEN)
 
 # get the repository object
 repo = g.get_repo(REPO_NAME)
-branch = "Python_Rpi"
+branch = "system"
 
 # get the GitTree object for the specified directory
 tree = repo.get_git_tree(sha=branch, recursive=True).tree
